f657e47650a16261b394d11f529c7ff60913daff
chrisrodz <christian.etpr10@gmail.com>
2013-07-13 01:18:14 -0700

    Save users to db

--- app.py
+++ app.py
@@ -19,9 +19,11 @@ saved_jobs = db.Table('saved_jobs',
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     access_token = db.Column(db.Text)
-    email = db.Column(db.Text)
-    expires_in = db.Column(db.DateTime)
-    saved_jobs = db.relationship('JobPosting', secondary='saved_jobs', backref=db.backref('pages', lazy='dynamic'))
+    email = db.Column(db.Text, unique=True)
+    jobs = db.relationship('Jobposting', secondary=saved_jobs, backref=db.backref('jobposting', lazy='dynamic'))
+
+    def __repr__(self):
+        return u'%s' % (self.email)
 
 
 class Jobposting(db.Model):
@@ -40,7 +42,7 @@ class Jobposting(db.Model):
 @app.route("/")
 def index():
     if 'access_token' in session:
-        return redirect(url_for('profile'))
+        return redirect(url_for('student_profile'))
     return render_template('login.html')
 
 
@@ -65,6 +67,12 @@ def student_profile():
         'university': 'CMU'
     }
     user = get_student_profile(profile.content)
+    email = User.query.filter_by(email=user['email']).count()
+    if not email:
+        newuser = User(access_token=token, email=user['email'])
+        db.session.add(newuser)
+        db.session.commit()
+        print 'user saved to db'
     return render_template('profile.html', user=user, job=job)
 
 
@@ -72,6 +80,7 @@ def student_profile():
 def logout():
     session.pop('access_token', None)
     return redirect(url_for('index'))
+
 
 @app.route('/index')
 def dashboard():
@@ -81,3 +90,4 @@ app.secret_key = '\xf8\x98\x80\xea\xde\xad\x9d\xf9\x90\xf58\x19\x062\x13]&f\x90\
 
 if __name__ == "__main__":
     app.run()
+
