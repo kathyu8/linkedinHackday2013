a4126349663c231b2e9461bfc3f2327339cd34d3
chrisrodz <christian.etpr10@gmail.com>
2013-07-13 03:59:47 -0700

    Added students can save jobs, and view jobs pertainig their universities

--- api.py
+++ api.py
@@ -2,12 +2,6 @@ import requests
 import json
 
 
-def get_access_token(code, redirect_uri):
-    r = requests.get('https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code='+code+'&redirect_uri='+redirect_uri+'&client_id=dkqwsero67sh&client_secret=GGxpjoa5b5NFCOxU')
-    response = json.loads(r.content)
-    return response['access_token']
-
-
 def get_student_profile(content):
     data = json.loads(content)
     user = {}
